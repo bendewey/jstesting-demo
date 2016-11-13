df7b901bbe0da7d7e2afc8a53b66d5dbba370551
Ben Dewey <bdewey01@hotmail.com>
2016-11-12 23:20:40 -0500

    upgraded ngBanking files to latest protrator

--- ngBanking_v2/tests/conf.js
+++ ngBanking_v2/tests/conf.js
@@ -1,4 +1,4 @@
 exports.config = {
-    seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
+    seleniumServerJar: '../node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
     specs: ['**\*.ui-test.js']
 };
